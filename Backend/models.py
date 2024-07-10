f5707e5ee4cabf6c56040aea75f00377b5075cd3
Christopher Cantu <chrischimi02@gmail.com>
2024-07-10 17:55:35 -0500

    model/API changes, added embed

--- Backend/models.py
+++ Backend/models.py
@@ -8,18 +8,18 @@ from config import db
 # Models go here!
 
 class Album(db.Model, SerializerMixin):
-    
     __tablename__ = 'albums'
     serialize_rules = ('-reviews',)
     
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     artist = db.Column(db.String(100), nullable=False)
-    genre = db.Column(db.String(50), nullable=False)
+    genre = db.Column(db.String(50), nullable=True)
     release_date = db.Column(db.Date, nullable=False)
     cover_image = db.Column(db.String(255), nullable=True)
+    embed_link = db.Column(db.String(255), nullable=True)
     reviews = db.relationship('Review', backref='album', lazy=True)
-    
+
 
 class Member(db.Model, SerializerMixin):
     __tablename__ = 'members'
@@ -28,9 +28,9 @@ class Member(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    join_date = db.Column(db.DateTime, default=func.now())
+    join_date = db.Column(db.DateTime, default=func.now(), nullable=False)
     reviews = db.relationship('Review', backref='member', lazy=True)
-    
+
 
 class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
@@ -38,6 +38,12 @@ class Review(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     album_id = db.Column(db.Integer, db.ForeignKey('albums.id'), nullable=False)
     member_id = db.Column(db.Integer, db.ForeignKey('members.id'), nullable=False)
-    review_date = db.Column(db.DateTime, default=func.now())
+    review_date = db.Column(db.DateTime, default=func.now(), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     comment = db.Column(db.Text, nullable=False)
+    
+    def __init__(self, album_id, member_id, rating, comment):
+        self.album_id = album_id
+        self.member_id = member_id
+        self.rating = max(1, min(5, rating))
+        self.comment = comment
