cb45c212067494c682f92a465c83a85b067bcc8b
Christopher Cantu <chrischimi02@gmail.com>
2024-07-11 21:40:42 -0500

    fixed the merge errors

--- Frontend/src/components/App.js
+++ Frontend/src/components/App.js
@@ -1,37 +1,16 @@
 // App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Albums from "./AlbumList";
-import Members from "./Members";
-import Reviews from "./Reviews";
-
-import "./App.css"; // Import your CSS file
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul className="nav-list">
-            <li className="nav-item">
-              <Link className="nav-link" to="/albums">Albums</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/members">Members</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/reviews">Reviews</Link>
-            </li>
-          </ul>
-        </nav>
-    
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AlbumDetails from "./AlbumDetails";
 import Login from "./Login";
 import Register from "./Register";
 import CheckLogin from "./CheckLogin";
 import LogOut from "./LogOut";
 import Profile from "./Profile";
-import "./App.css";
+import Members from "./Members";
+import Reviews from "./Reviews";
+import Albums from "./AlbumList";
+import Navbar from "./NavBar";
 
 import { AuthProvider } from "./AuthContext";
 
@@ -39,44 +18,19 @@ function App() {
   return (
     <AuthProvider>
       <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/albums">Albums</Link>
-              </li>
-              <li>
-                <Link to="/members">Members</Link>
-              </li>
-              <li>
-                <Link to="/reviews">Reviews</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/logout">Logout</Link>
-              </li>
-              <li>
-                <Link to="/Profile">Profile</Link>
-              </li>
-            </ul>
-          </nav>
-
-
-          <Switch>
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/logout" component={LogOut} />
-            <Route path="/albums/:id" component={AlbumDetails} />
-            <Route path="/albums" exact component={Albums} />
-            <Route path="/members" component={Members} />
-            <Route path="/reviews" component={Reviews} />
-            <Route path="/check_login" component={CheckLogin} />
-            <Route path="/" exact component={Albums} />
-            <Route path="/profile" component={Profile} />
-          </Switch>
-        </div>
+        <Navbar />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/logout" component={LogOut} />
+          <Route path="/albums/:id" component={AlbumDetails} />
+          <Route path="/albums" exact component={Albums} />
+          <Route path="/members" component={Members} />
+          <Route path="/reviews" component={Reviews} />
+          <Route path="/check_login" component={CheckLogin} />
+          <Route path="/" exact component={Albums} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
       </Router>
     </AuthProvider>
   );
---
+++ Frontend/src/components/NavBar.js
@@ -0,0 +1,45 @@
+// Navbar.js
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link className="nav-link" to="/albums">
+            Albums
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/members">
+            Members
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/reviews">
+            Reviews
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/logout">
+            Logout
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/profile">
+            Profile
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
